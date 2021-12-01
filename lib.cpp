755222f3ce7f2662c67ce75e1f92fdc94c51e622
AndreaBarbazza <91791228+AndreaBarbazza@users.noreply.github.com>
2021-12-01 19:16:55 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include "lib.h"
 
+bool NumeroPrimo(int a,int b){
+
+    if (a<2){
+        return false;
+    }
+    if (a==2){
+        return true;
+    }
+    int risultato= a%b;
+    if (risultato==0){
+        return false;
+    }
+
+    if (b>2){
+        bool result= NumeroPrimo(a,b-1);
+        return result;
+    }
+    return true;
+}
